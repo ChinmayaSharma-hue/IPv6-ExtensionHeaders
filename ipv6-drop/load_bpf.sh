f58a6010fc53da73dc9a1f1943f81f5f0ef954f1
Balajinaidu V <72434180+ba1ajinaidu@users.noreply.github.com>
2023-03-20 12:19:39 +0530

    Typo
    Fix typo in loader

--- ipv6-drop/load_bpf.sh
+++ ipv6-drop/load_bpf.sh
@@ -4,7 +4,7 @@ if [ $# -ne 1 ]; then
     echo "Usage: $0 <interface>"
     exit 1
 fi
-sudo tc qdisc add dev $0 clsact
-sudo tc filter add dev $0 egress bpf direct-action obj ipv6_drop.o sec classifier
-sudo tc filter show dev $0
-sudo tc filter show dev $0 egress
+sudo tc qdisc add dev $1 clsact
+sudo tc filter add dev $1 egress bpf direct-action obj ipv6_drop.o sec classifier
+sudo tc filter show dev $1
+sudo tc filter show dev $1 egress
